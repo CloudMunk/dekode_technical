12787add0caf239730520fef4397e5fbf20da5e9
Anestis Finstad <munkeyten@protonmail.com>
2020-08-22 00:04:21 +0200

    Landing Page layout
    Began setting up elements and sections of landing page for desktop.

--- src/components/homepage/homepage.component.jsx
+++ src/components/homepage/homepage.component.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 
+import rightArrowSmall from '../../assets/arrowSmallRight.svg'
+import janeDoe from '../../assets/janeDoe.png'
+
 import './homepage.styles.scss'
 
 const Homepage = () => (
-    <div className="homepageContainer">
-        <h1>This is the homepage</h1>
+    <div className="autoLayout">
+        <div className="frame13">
+            <p className="teft">
+                teft
+            </p>
+            <img src={rightArrowSmall} alt="Small pruple arrow pointing right" className="forwardFirst" />
+            <p className="circlesHome">
+                Circles
+            </p>
+            <img src={rightArrowSmall} alt="Small pruple arrow pointing right" className="forwardSecond" />
+            <p className="purpleCircles">
+                Purple Circles
+            </p>
+        </div>
+       <h1 className="pageHeader">
+            Purple Circles
+       </h1>
+       <p className="purpleAssociated">
+        Purple is closely associated with violet. In optics, purple and violet refer to colors that 
+        look similar, but purples are mixtures of red and blue or violet light.
+       </p>
+       <div className="frame28">
+       <img src={janeDoe} className="janeDoe" alt="Jane Doe worker"/>
+           <div className="frame26">
+                <p className="janeDoeName">
+                    Jane Doe
+                </p>
+                <p className="metatekstDesktop">
+                    Projectmanager Dekode – 5 min read – 2
+                </p>
+           </div>
+           
+       </div>
     </div>
 )
 
